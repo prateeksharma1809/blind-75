1a1fc4410ff03ecc9519f354e0ecf8ecbdb49d48
prateeksharma18 <ps2798@rit.edu>
2024-06-11 19:45:06 -0400

    Top k elements

---
+++ README.md
@@ -0,0 +1,3 @@
+https://leetcode.com/problems/two-sum/description/ -> TwoSum.java
+https://leetcode.com/problems/valid-anagram/description/ -> ValidAnagram.java
+https://leetcode.com/problems/top-k-frequent-elements/description/  -> TopKelements.java
---
+++ src/main/java/com/leetcode/arrays/TopKelements.java
@@ -0,0 +1,46 @@
+package com.leetcode.arrays;
+
+import java.util.*;
+
+public class TopKelements {
+
+	class Solution {
+		public int[] topKFrequent(int[] nums, int k) {
+			HashMap<Integer, Integer> map = new HashMap<>();
+			for (int i : nums) {
+				map.put(i, map.getOrDefault(i, 0) + 1);
+			}
+			List<List<Integer>> list = new ArrayList<>();
+			for(int i =0; i <nums.length+1;i++) {
+				list.add(new ArrayList<>());
+			}
+			for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+				List<Integer> l = list.get(entry.getValue());
+				l.add(entry.getKey());
+			}
+			int[] result = new int[k];
+			int index=0;
+			for(int i=list.size()-1;i>=0;i--) {
+				for(Integer j : list.get(i)) {
+					result[index]=j;
+					index++;
+					if(index==k) {
+						return result;
+					}
+				}
+			}
+
+			return nums;
+		}
+	}
+
+	public static void main(String[] args) {
+		TopKelements tp = new TopKelements();
+		Solution s = tp.new Solution();
+		for(int i :s.topKFrequent(new int[] {1,2,2,3,3,3}, 2)) {
+			System.out.println(i);
+		}
+
+	}
+
+}
